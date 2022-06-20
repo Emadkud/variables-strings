b5075f176986854bb2bf3e11f89d264016407ce8
Emad0007 <BASEMRF007@GMAIL.COM>
2022-06-20 14:16:00 +0100

    finly js

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 'use strict'
+
+//console.log("Hello world !")//
+
+
+
+/*const sentence = "My name is Emad"
+console.log(sentence) */
+
+
+/*const Name= "Emad"
+const sentence="Nice to meet you"
+console.log(`${sentence} ${Name}`)*/
+
+
+/*const sentence = "i'm a new text!"
+console.log(sentence.length)*/
+
+
+
+/*const food = "croissant is meh"
+console.log (food.replace("meh" , "good"))*/
+
+
+/*const basic = "This is cool"
+const basicUp = basic.toUpperCase()
+const basicDown =basicUp.toLowerCase()
+
+console.log(basic, basicUp, basicDown)*/
+
+
+/*const word = "banana"
+const letters = word.split("")
+console.log(letters)*/
+
+/*const age ="18"
+const template =`I'm ${age} years old`
+console.log(template)*/
+
+const changed = "Bonjour"
+console.log(changed.replaceAll("o" ,"a"))
